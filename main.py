a003d549e90a49a61e4c30d5f7638c865bc21d79
Sai Avala <avala.sai@gmail.com>
2016-03-04 23:48:37 -0500

    Added more price metrics

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ app = Flask(__name__)
 @app.route('/stock')
 def stock():
     ticker = request.args.get('text')
-    url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1"
-    result = requests.get(url)
-    return ticker.upper() + ": $" + str(result.json())
+    # Get price, amount change, percent change, day high, day low
+    url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1c1p2hg"
+    result = requests.get(url).text.split(',')
+    result[2] = result[2].replace('"','')
+    result[4] = result[4].replace('\n','')
+    result_text = ("*" + ticker.upper() + "*" + "\n--------\n"
+                    "Price: " + result[0] + "\n"
+                    "Amt Change: " + result[1] + "\n"
+                    "% Change: " + result[2] + "\n"
+                    "Day High: " + result[3] + "\n"
+                    "Day Low: " + result[4])
+    return result_text
 
 if __name__ == '__main__':
     app.debug = True
