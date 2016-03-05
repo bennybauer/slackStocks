4996a640a0094fa2f7c66e056eb77978f303e3d5
Sai Avala <avala.sai@gmail.com>
2016-03-04 23:21:01 -0500

    Made some quick changes

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
 @app.route('/stock')
 def stock():
     ticker = request.args.get('ticker')
     url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1"
     result = requests.get(url)
-    return ticker.upper() + ": " + str(result.json())
+    return ticker.upper() + ": $" + str(result.json())
 
 if __name__ == '__main__':
     app.debug = True
