7f117bb2efe3b0d2249c17006803a2a272402f19
Sai Avala <avala.sai@gmail.com>
2016-03-08 22:24:06 -0500

    Added try catch error handling

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ app = Flask(__name__)
 
 @app.route('/stock')
 def stock():
-    ticker = request.args.get('text')
-    # Get price, amount change, percent change, day high, day low
-    url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1c1p2hg"
-    result = requests.get(url).text.split(',')
-    result[2] = result[2].replace('"','')
-    result[4] = result[4].replace('\n','')
-    result_text = ("*" + ticker.upper() + "*" + "\n"
-                    "Price: " + result[0] + "\n"
-                    "Amt Change: " + result[1] + "\n"
-                    "% Change: " + result[2] + "\n"
-                    "Day High: " + result[3] + "\n"
-                    "Day Low: " + result[4])
+    result_text = ""
+    try:
+        ticker = request.args.get('text')
+        # Get price, amount change, percent change, day high, day low
+        url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1c1p2hg"
+        result = requests.get(url).text.split(',')
+        result[2] = result[2].replace('"','')
+        result[4] = result[4].replace('\n','')
+        result_text = ("*" + ticker.upper() + "*" + "\n"
+                        "Price: " + result[0] + "\n"
+                        "Amt Change: " + result[1] + "\n"
+                        "% Change: " + result[2] + "\n"
+                        "Day High: " + result[3] + "\n"
+                        "Day Low: " + result[4])
+    except Exception as e:
+        result_text = "Please enter in a valid ticker. ie /stock xyz"
     return result_text
 
 if __name__ == '__main__':
